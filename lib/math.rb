f3b63b7e10d6776620331a4efe927405a944ee8a
Vannida Lim <vannida731@gmail.com>
2019-09-09 08:22:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,21 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
 end
 
 def modulo(num1, num2)
-
+  quotient = num1 % num2
 end
 
 def square_root(num)
-
+  result = num * num
 end
